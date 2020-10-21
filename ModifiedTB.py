ccd55f43015d8186aa0ec4a7fe3b33b5ef34bfb5
Richard Yang <im.richardryang@gmail.com>
2020-10-21 12:56:44 -0700

    modified tensorboard

---
+++ ModifiedTB.py
@@ -0,0 +1,25 @@
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+
+
+class ModifiedTB(TensorBoard):
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+
+    def set_model(self, model):
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    def on_train_end(self, _):
+        pass
+
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
